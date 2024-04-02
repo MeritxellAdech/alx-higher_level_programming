f00c711f5412416e7d1d4335d4505ea3417c06b9
Merit <meritxelladech@gmail.com>
2024-04-02 22:57:01 +0000

    ADD: giving a size to the square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This module defines a square
+"""
+
+
+class Square:
+    """
+    Definition of the square class
+    """
+
+    def __init__(self, size):
+        """
+        This is the method initializer of all variables
+
+        :args:
+            self: this the class itself
+            size: the give size of the square
+        """
+        self.__size = size
