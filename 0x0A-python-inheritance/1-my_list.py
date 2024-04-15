c55093acc5343ce0fa3c5f765a8937d645356b6d
Merit <meritxelladech@gmail.com>
2024-04-15 21:05:15 +0000

    add: create a copy of a list and prints the elements in in ascending order

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Contains a class that inherits from a list"""
+
+
+class MyList(list):
+    """ A subclass of a list class and print the elements"""
+
+    def print_sorted(self):
+        """
+        prints the list of sorted elements
+        """
+        new_list = self.copy()
+        mew_list.sort()
+        print(new_list)
