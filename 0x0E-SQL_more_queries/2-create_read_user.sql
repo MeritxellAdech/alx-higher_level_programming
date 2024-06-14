bee4808b30b7df66687a0d3a342e862e6cef9df4
Merit <meritxelladech@gmail.com>
2024-06-14 16:41:20 +0100

    Granting usage to user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,5 +4,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creating a user
 CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" IDENTIFIED BY 'user_0d_2_pwd';
 
--- Granting user_0d_2 SELECT permission on hbtn_0d_2 
-GRANT SELECT ON hbtn_0d_2.* TO 'hbtn_0d_2'@'localhost';
+-- Ensuring that the user account is valid
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+
+-- Granting user_0d_2 SELECT permission on hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
