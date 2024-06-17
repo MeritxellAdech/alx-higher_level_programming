7be6620542c8ab80683cc325ce58df889b32cc98
Merit <meritxelladech@gmail.com>
2024-06-17 19:30:08 +0100

    Updating the join condition

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
--- Retrives all shows that do not have a genre
+-- Retrives all shows without genre
 
 SELECT title, genre_id
 FROM tv_shows s LEFT JOIN tv_show_genres g
-ON s.id = g.genre_id
+ON id = show_id
 WHERE genre_id IS NULL
 ORDER BY title ASC, genre_id ASC;
