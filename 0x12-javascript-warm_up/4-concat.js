9f92a325d52e758863dc592c1552016b70d935e4
Merit <meritxelladech@gmail.com>
2024-06-18 17:22:30 +0100

    Displaying a custom message based on the number of arguments passed

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+if (argv[2] && argv[3]) {
+  console.log(`${argv[2]} is ${argv[3]}`);
+} else if (!argv[2] && argv[3]) {
+  console.log(`${argv[2]} is ${argv[3]}`);
+} else {
+  console.log(`${argv[2]} is ${argv[3]}`);
+}
