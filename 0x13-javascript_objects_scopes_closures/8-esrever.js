0b86f67964fbabc6c4cfaea935feb4ee738be241
Merit <meritxelladech@gmail.com>
2024-06-19 21:13:35 +0100

    Returning a reversed version of a list without using the reverse()

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,5 +2,10 @@
 
 // Anonymous function that returns the reversed version of a list
 exports.esrever = function (list) {
-  return (list.reverse());
+  const reversed = [];
+  let rev = list.length - 1;
+  for (let i = 0; i < list.length; i++) {
+    reversed[i] = list[rev--];
+  }
+  return (reversed);
 };
