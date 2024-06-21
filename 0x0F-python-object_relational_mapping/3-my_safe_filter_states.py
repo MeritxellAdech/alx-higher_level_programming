48ad833a95f93137d1deb26df025c05327e2d8db
Merit <meritxelladech@gmail.com>
2024-06-21 21:51:57 +0100

    SQL injection free query

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" Displaying states that matches given name """
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    USERNAME = argv[1]
+    PASS = argv[2]
+    DBNAME = argv[3]
+    data = argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=USERNAME,
+        passwd=PASS,
+        db=DBNAME,
+        charset="utf8"
+    )
+    cursor = conn.cursor()
+    QUERY = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
+    cursor.execute(QUERY, {data})
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
