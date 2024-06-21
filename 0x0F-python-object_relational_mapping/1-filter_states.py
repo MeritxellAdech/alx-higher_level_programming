2678e389ee8e8d7efb5a23545337ad4b456844a6
Merit <meritxelladech@gmail.com>
2024-06-21 20:42:15 +0100

    Displaying all names that begin with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""This module displays all states that begin with N """
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    USERNAME = argv[1]
+    PASS = argv[2]
+    DBNAME = argv[3]
+    con = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=USERNAME,
+        passwd=PASS,
+        db=DBNAME,
+        charset="utf8"
+        )
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    n_states = cur.fetchall()
+    for state in n_states:
+        print(state)
+    cur.close()
+    con.close()
